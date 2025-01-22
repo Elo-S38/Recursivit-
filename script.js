fa7f71f12f12b45a49a3c7bb197e40139d639ec3
Elo-S38 <elodie.sorce@gmail.com>
2025-01-22 15:49:27 +0100

    etape1 Ok

---
+++ script.js
@@ -0,0 +1,56 @@
+const dossierPrincipal = {
+  nom: "Ada",
+  contenu: [
+    {
+      nom: "Projets collectifs Ada",
+      contenu: [
+        {
+          nom: "Pico8",
+          contenu: [
+            {
+              nom: "mariokart.p8",
+            },
+          ],
+        },
+        {
+          nom: "Dataviz",
+          contenu: [
+            {
+              nom: "index.html",
+            },
+            {
+              nom: "script.js",
+            },
+          ],
+        },
+      ],
+    },
+    { nom: "CV.pdf" },
+    {
+      nom: "Projets persos",
+      contenu: [
+        {
+          nom: "Portfolio",
+          contenu: [
+            {
+              nom: "index.html",
+            },
+            {
+              nom: "script.js",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+// console.log(dossierPrincipal);
+
+//---------------------Etape 1------------------
+//fonction afficherDossier qui va afficher le nom du dossier principal en utilisant la variable dossierPrincipal
+function afficherDossier(dossier) {
+  console.log(dossier.nom);
+}
+
+afficherDossier(dossierPrincipal);
