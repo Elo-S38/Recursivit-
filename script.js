542ff4a3ef74517a489bf6d5fbc64063f2b3582e
Elo-S38 <elodie.sorce@gmail.com>
2025-01-26 15:00:39 +0100

    Etape 3 function recursive ok (pas iterative)

--- script.js
+++ script.js
@@ -79,3 +79,33 @@ function afficherDossierIteratif(dossier) {
 afficherDossierIteratif(dossierPrincipal);
 
 //fonction afficherDossierRecursif
+
+//-----------------Etape 3------------------------
+//Complète la fonction pour afficher les dossiers de troisième niveau (ex: les dossiers “Pico 8” et “Dataviz” sont des dossiers de troisième niveau car ils se trouvent dans un dossier “Projets collectifs, lui-même dans le dossier “Ada”).
+
+function afficherDossierIteratif2(dossier) {
+  const pile = [dossier]; // On utilise une pile pour parcourir tous les dossiers et fichiers
+
+  while (pile.length > 0) {}
+}
+
+afficherDossierIteratif2(dossierPrincipal);
+
+// fonction recursive pour afficher les dossiers de 3eme niveau
+function afficherDossierRecursif(dossier) {
+  console.log(dossier.nom); // Affiche le nom du dossier
+
+  if (dossier.contenu) {
+    for (const elt of dossier.contenu) {
+      if (elt.contenu) {
+        // Appel récursif pour afficher les sous-dossiers
+        afficherDossierRecursif(elt);
+      } else {
+        // Affiche les fichiers
+        console.log(elt.nom);
+      }
+    }
+  }
+}
+
+afficherDossierRecursif(dossierPrincipal);
